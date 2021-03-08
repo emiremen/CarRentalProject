a2816bd71e4695a302a25ecb940464b31028abf8
emir emen <websevant@hotmail.com>
2021-03-09 00:15:32 +0300

    Web API Added

---
+++ WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService;
+
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet("getall")] //localhost:44306/api/cars/getall
+        public IActionResult GetAll()
+        {
+            var result = _carService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        } 
+
+        [HttpGet("getbyid")] //localhost:44306/api/cars/getbyid?id=4
+        public IActionResult GetById(int id)
+        {
+            var result = _carService.GetCarById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("addcar")] //localhost:44306/api/cars/addcar
+        public IActionResult AddCar(Car car)
+        {
+            var result = _carService.Add(car);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
